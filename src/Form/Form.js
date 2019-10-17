2ff797687aa14295702a54fb568da6e9ad7dc37f
Eric Meldrum <eric.meldrum@gmail.com>
2019-10-17 10:23:01 -0600

    Create handleChange method and add it to each Form input

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,17 +4,31 @@ import './Form.css';
 
 class Form extends Component {
   constructor() {
-    super()
+    super();
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  updateState = () => {
+    e.preventDefault();
+
   }
 
   render() {
-
     return (
       <div>
-        <input name='name' type='text' placeholder='Name'/>
-        <input name='date' type='text' placeholder='Date'/>
-        <input name='time' type='text' placeholder='Time'/>
-        <input name='guests' type='number' placeholder='Number of guests'/>
+        <input name='name' value={this.state.name} type='text' placeholder='Name' onChange={this.handleChange}/>
+        <input name='date' value={this.state.date} type='text' placeholder='Date' onChange={this.handleChange}/>
+        <input name='time' value={this.state.time} type='text' placeholder='Time' onChange={this.handleChange}/>
+        <input name='guests' value={this.state.number} type='number' placeholder='Number of guests' onChange={this.handleChange}/>
         <button>Make reservation</button>
       </div>
     )
